1c61eca9ccb5e99c56476e2a9f74240946d9bcd6
Alec Wells <alec.wells47@gmail.com>
2020-03-19 14:43:47 -0600

    Finish clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -15,4 +15,12 @@ class Clock
   def +(clock)
     Clock.new(hour: (clock.hour + @hour), minute: (clock.minute + @minute))
   end
+
+  def -(clock)
+    Clock.new(hour: (@hour - clock.hour), minute: (@minute - clock.minute))
+  end
+
+  def ==(clock)
+    @hour == clock.hour && @minute == clock.minute
+  end
 end
