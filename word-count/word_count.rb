6df2234b543da98e75899708a2b5169c76252079
Alec Wells <alec.wells47@gmail.com>
2020-03-20 16:25:09 -0600

    Finish word_count

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,12 +1,12 @@
 class Phrase
 
   def initialize(phrase)
-    @phrase = phrase.split(/[^\w-]+/)
+    @phrase = phrase.downcase.scan(/\w+'?\w|\w/)
   end
 
   def word_count
     word_hash = Hash.new(0)
-    @phrase.each {|word| word_hash[word.downcase] += 1}
+    @phrase.each {|word| word_hash[word] += 1}
     word_hash
   end
 end
