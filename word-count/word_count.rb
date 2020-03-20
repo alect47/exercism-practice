e7ed6dbe96ad292611b4c9b102490e8b5637f7c7
Alec Wells <alec.wells47@gmail.com>
2020-03-20 12:11:52 -0600

    Create Phrase class

---
+++ word-count/word_count.rb
@@ -0,0 +1,12 @@
+class Phrase
+
+  def initialize(phrase)
+    @phrase = phrase.split
+  end
+
+  def word_count
+    word_hash = Hash.new(0)
+    @phrase.each {|word| word_hash[word] += 1}
+    word_hash
+  end
+end
